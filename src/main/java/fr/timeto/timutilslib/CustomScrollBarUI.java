07d462fa4fb9f30d5ab85d433874f6fdf7b7ed9c
TimEtOff <timeogodefroy@gmail.com>
2023-04-21 16:11:01 +0200

    Color change for scroll bar

--- src/main/java/fr/timeto/timutilslib/CustomScrollBarUI.java
+++ src/main/java/fr/timeto/timutilslib/CustomScrollBarUI.java
@@ -6,14 +6,214 @@ import javax.swing.plaf.basic.BasicScrollBarUI;
 import java.awt.*;
 
 public class CustomScrollBarUI extends BasicScrollBarUI {
+
+    protected static Color staticTrackColor = new Color(25, 25, 25);
+    protected static Color staticTrackHighlightColor = new Color(30, 30, 30);
+    protected static Color staticThumbColor = new Color(30, 30, 30);
+    protected static Color staticThumbDarkShadowColor = new Color(30, 30, 30);
+    protected static Color staticThumbLightShadowColor = new Color(30, 30, 30);
+    protected static Color staticThumbHighlightColor = new Color(30, 30, 30);
+    protected static Color staticButtonBackgroundColor = new Color(30,30,30);
+    protected static Color staticButtonShadowColor = Color.RED.darker();
+    protected static Color staticButtonDarkShadowColor = Color.RED;
+    protected static Color staticButtonHighlightColor = new Color(30,30,30).brighter();
+
     @Override
     protected void configureScrollBarColors() {
-        this.trackColor = new Color(25, 25, 25);
-        this.trackHighlightColor = new Color(40, 40, 40);
-        this.thumbColor = new Color(40, 40, 40);
-        this.thumbDarkShadowColor = new Color(40, 40, 40);
-        this.thumbLightShadowColor = new Color(40, 40, 40);
-        this.thumbHighlightColor = new Color(40, 40, 40);
+        this.trackColor = staticTrackColor;
+        this.trackHighlightColor = staticTrackHighlightColor;
+        this.thumbColor = staticThumbColor;
+        this.thumbDarkShadowColor = staticThumbDarkShadowColor;
+        this.thumbLightShadowColor = staticThumbLightShadowColor;
+        this.thumbHighlightColor = staticThumbHighlightColor;
+    }
+
+    public void recolor() {
+        configureScrollBarColors();
+
+        switch (scrollbar.getOrientation()) {
+            case JScrollBar.VERTICAL:
+                incrButton = createIncreaseButton(SOUTH);
+                decrButton = createDecreaseButton(NORTH);
+                break;
+
+            case JScrollBar.HORIZONTAL:
+                if (scrollbar.getComponentOrientation().isLeftToRight()) {
+                    incrButton = createIncreaseButton(EAST);
+                    decrButton = createDecreaseButton(WEST);
+                } else {
+                    incrButton = createIncreaseButton(WEST);
+                    decrButton = createDecreaseButton(EAST);
+                }
+                break;
+        }
+    }
+
+    /**
+     * Change colors of the scroll bar
+     * @param trackColor Original 25-25-25
+     * @param elementColor Original 30-30-30
+     * @param mainColor Original RED
+     * @see #recolor(Color, Color, Color, Color, Color, Color, Color, Color, Color, Color)
+     */
+    public void recolor(Color trackColor,
+                        Color elementColor,
+                        Color mainColor) {
+        this.recolor(trackColor, elementColor, elementColor, elementColor, elementColor, elementColor, elementColor, mainColor.darker(), mainColor, elementColor.brighter());
+    }
+
+    /**
+     * Change colors of the scroll bar. If a color is null, no change
+     * @param trackColor Original 25-25-25
+     * @param trackHighlightColor Original 30-30-30
+     * @param thumbColor Original 30-30-30
+     * @param thumbDarkShadowColor Original 30-30-30
+     * @param thumbLightShadowColor Original 30-30-30
+     * @param thumbHighlightColor Original 30-30-30
+     * @param buttonBackgroundColor Original 30-30-30
+     * @param buttonShadowColor Original RED.darker()
+     * @param buttonDarkShadowColor Original RED
+     * @param buttonHighlightColor Original 30-30-30.brighter
+     */
+    public void recolor(Color trackColor,
+                        Color trackHighlightColor,
+                        Color thumbColor,
+                        Color thumbDarkShadowColor,
+                        Color thumbLightShadowColor,
+                        Color thumbHighlightColor,
+                        Color buttonBackgroundColor,
+                        Color buttonShadowColor,
+                        Color buttonDarkShadowColor,
+                        Color buttonHighlightColor) {
+
+        if (trackColor != null) {
+            this.trackColor = trackColor;
+        }
+        if (trackHighlightColor != null) {
+            this.trackHighlightColor = trackHighlightColor;
+        }
+        if (thumbColor != null) {
+            this.thumbColor = thumbColor;
+        }
+        if (thumbDarkShadowColor != null) {
+            this.thumbDarkShadowColor = thumbDarkShadowColor;
+        }
+        if (thumbLightShadowColor != null) {
+            this.thumbLightShadowColor = thumbLightShadowColor;
+        }
+        if (thumbHighlightColor != null) {
+            this.thumbHighlightColor = thumbHighlightColor;
+        }
+
+        switch (scrollbar.getOrientation()) {
+            case JScrollBar.VERTICAL:
+                incrButton = new BasicArrowButton(SOUTH,
+                        buttonBackgroundColor,
+                        buttonShadowColor,
+                        buttonDarkShadowColor,
+                        buttonHighlightColor);
+                decrButton = new BasicArrowButton(NORTH,
+                        buttonBackgroundColor,
+                        buttonShadowColor,
+                        buttonDarkShadowColor,
+                        buttonHighlightColor);
+                break;
+
+            case JScrollBar.HORIZONTAL:
+                if (scrollbar.getComponentOrientation().isLeftToRight()) {
+                    incrButton = new BasicArrowButton(EAST,
+                            buttonBackgroundColor,
+                            buttonShadowColor,
+                            buttonDarkShadowColor,
+                            buttonHighlightColor);
+                    decrButton = new BasicArrowButton(WEST,
+                            buttonBackgroundColor,
+                            buttonShadowColor,
+                            buttonDarkShadowColor,
+                            buttonHighlightColor);
+                } else {
+                    incrButton = new BasicArrowButton(WEST,
+                            buttonBackgroundColor,
+                            buttonShadowColor,
+                            buttonDarkShadowColor,
+                            buttonHighlightColor);
+                    decrButton = new BasicArrowButton(EAST,
+                            buttonBackgroundColor,
+                            buttonShadowColor,
+                            buttonDarkShadowColor,
+                            buttonHighlightColor);
+                }
+                break;
+        }
+    }
+
+    /**
+     * Change colors of the scroll bar
+     * @param trackColor Original 25-25-25
+     * @param elementColor Original 30-30-30
+     * @param mainColor Original RED
+     * @see #staticRecolor(Color, Color, Color, Color, Color, Color, Color, Color, Color, Color)
+     */
+    public static void staticRecolor(Color trackColor,
+                        Color elementColor,
+                        Color mainColor) {
+        staticRecolor(trackColor, elementColor, elementColor, elementColor, elementColor, elementColor, elementColor, mainColor.darker(), mainColor, elementColor.brighter());
+    }
+
+    /**
+     * Change colors of the scroll bar. If a color is null, no change
+     * @param trackColor Original 25-25-25
+     * @param trackHighlightColor Original 30-30-30
+     * @param thumbColor Original 30-30-30
+     * @param thumbDarkShadowColor Original 30-30-30
+     * @param thumbLightShadowColor Original 30-30-30
+     * @param thumbHighlightColor Original 30-30-30
+     * @param buttonBackgroundColor Original 30-30-30
+     * @param buttonShadowColor Original RED.darker()
+     * @param buttonDarkShadowColor Original RED
+     * @param buttonHighlightColor Original 30-30-30.brighter
+     */
+    public static void staticRecolor(Color trackColor,
+                        Color trackHighlightColor,
+                        Color thumbColor,
+                        Color thumbDarkShadowColor,
+                        Color thumbLightShadowColor,
+                        Color thumbHighlightColor,
+                        Color buttonBackgroundColor,
+                        Color buttonShadowColor,
+                        Color buttonDarkShadowColor,
+                        Color buttonHighlightColor) {
+       
+        if (trackColor != null) {
+            staticTrackColor = trackColor;
+        }
+        if (trackHighlightColor != null) {
+            staticTrackHighlightColor = trackHighlightColor;
+        }
+        if (thumbColor != null) {
+            staticThumbColor = thumbColor;
+        }
+        if (thumbDarkShadowColor != null) {
+            staticThumbDarkShadowColor = thumbDarkShadowColor;
+        }
+        if (thumbLightShadowColor != null) {
+            staticThumbLightShadowColor = thumbLightShadowColor;
+        }
+        if (thumbHighlightColor != null) {
+            staticThumbHighlightColor = thumbHighlightColor;
+        }
+        if (buttonBackgroundColor != null) {
+            staticButtonBackgroundColor = buttonBackgroundColor;
+        }
+        if (buttonShadowColor != null) {
+            staticButtonShadowColor = buttonShadowColor;
+        }
+        if (buttonDarkShadowColor != null) {
+            staticButtonDarkShadowColor = buttonDarkShadowColor;
+        }
+        if (buttonHighlightColor != null) {
+            staticButtonHighlightColor = buttonHighlightColor;
+        }
     }
 
     protected JButton createZeroButton() {
@@ -28,18 +228,18 @@ public class CustomScrollBarUI extends BasicScrollBarUI {
     @Override
     protected JButton createDecreaseButton(int orientation) {
         return new BasicArrowButton(orientation,
-                new Color(40, 40, 40),
-                new Color(40, 40, 40),
-                Color.RED,
-                new Color(40, 40, 40));
+                staticButtonBackgroundColor,
+                staticButtonShadowColor,
+                staticButtonDarkShadowColor,
+                staticButtonHighlightColor);
     }
 
     @Override
     protected JButton createIncreaseButton(int orientation) {
         return new BasicArrowButton(orientation,
-                new Color(40, 40, 40),
-                new Color(40, 40, 40),
-                Color.RED,
-                new Color(40, 40, 40));
+                staticButtonBackgroundColor,
+                staticButtonShadowColor,
+                staticButtonDarkShadowColor,
+                staticButtonHighlightColor);
     }
 }
